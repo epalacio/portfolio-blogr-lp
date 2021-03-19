b87e0ca30ff12eeeb1eabe1db0d38908d2929214
Esteban Palacio <estipalacio@gmail.com>
2021-03-19 09:07:01 -0400

    did cleanup of the react app, and tested tailwindCSS

--- src/App.js
+++ src/App.js
@@ -1,25 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Fragment } from 'react';
+import './index.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p className=" text-6xl text-red-900">
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => (
+//Fragment is temporary before setting up router
+  <Fragment>
+    <h1 className="text-red-900 text-6xl">Hello World</h1>
+  </Fragment>
+);
 
 export default App;
