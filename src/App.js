c48a333999296022ca296d533aad69e77d6e8a02
Esteban Palacio <estipalacio@gmail.com>
2021-03-21 21:54:54 -0400

    Added test for tailwind

--- src/App.js
+++ src/App.js
@@ -5,6 +5,9 @@ const App = () => (
 //Fragment is temporary before setting up router
   <Fragment>
     <h1 className="text-red-900 text-6xl">Hello World</h1>
+    <div>
+      <p className="text-blue-900">Testing text for tailwindCSS</p>
+    </div>
   </Fragment>
 );
 
