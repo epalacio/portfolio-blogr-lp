37ef564488f5b74b1600d4fa414949fcc8123851
Esteban Palacio <estipalacio@gmail.com>
2021-03-23 09:17:21 -0400

    added tests

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React, { Fragment } from 'react';
 import './index.css';
+import Hero from './components/Hero';
+import Features from './components/Features';
 
 const App = () => (
 //Fragment is temporary before setting up router
@@ -7,6 +9,8 @@ const App = () => (
     <h1 className="text-red-900 text-6xl">Hello World</h1>
     <div>
       <p className="text-blue-900">Testing text for tailwindCSS</p>
+      <Hero/>
+      <Features/>
     </div>
   </Fragment>
 );
