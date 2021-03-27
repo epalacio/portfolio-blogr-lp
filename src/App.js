c85471810cfe263db19a0df8749fad31e809784f
Esteban Palacio <estipalacio@gmail.com>
2021-03-26 21:28:00 -0400

    added two more components to main app

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
 import React, { Fragment } from 'react';
 import './index.css';
+import Nav from './components/Nav';
+import Infrastructure from './components/Infrastructure'
 import Hero from './components/Hero';
 import Features from './components/Features';
+import Future from './components/Future'
 
 const App = () => (
 //Fragment is temporary before setting up router
@@ -9,7 +12,10 @@ const App = () => (
     <h1 className="text-red-900 text-6xl">Hello World</h1>
     <div>
       <p className="text-blue-900">Testing text for tailwindCSS</p>
+      <Nav/>
+      <Infrastructure/>
       <Hero/>
+      <Future/>
       <Features/>
     </div>
   </Fragment>
