37dcc6bd2846f98d0b13caf6a972f70b42f27638
Esteban Palacio <estipalacio@gmail.com>
2021-09-26 11:33:09 -0400

    add colours, fontSizes, favicon

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,31 @@ module.exports = {
   purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'carnation': '#ff525d',
+        'fadedRed': '#ff7a85',
+        'regalBlue': '#1f3f5b',
+        'white': '#ffffff',
+        'iron': '#c8c8cb',
+        'fiord': '#4b5862',
+        'blackRussian': '#25252d',
+        'salmon': '#ff8f70',
+        'radicalRed': '#ff3d54',
+        'valhalla': '#2c2d3f',
+        'portGore': '#3f4164'
+      }
+    },
+    fontSize: {
+      'heroHeaderMobile': '36px',
+      'heroHeaderDesktop': '64px',
+      'heroSubHeaderMobile': '18px',
+      'heroSubHeaderDesktop': '20px',
+      'subHeaderMobile': '28px',
+      'subHeaderDesktop': '40px',
+      'h3Header': '28px',
+      'baseText': '16px'
+    }
   },
   variants: {
     extend: {},
